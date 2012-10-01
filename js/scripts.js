9458011a68831d1741ddadc3c9440c63aa7e90d1
Nicholas Murray <murray.nicholas@gmail.com>
2012-10-01 18:47:01 +0100

    Added Index Page and Script

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var ratPack = $.sammy(function() {
+
+    this.element_selector = '#content';
+
+    $(function() {
+	  ratPack.run('#/view_league');
+	});
+    
+    // routes
+	this.get('#/view_league', function(context) {
+	    context.app.swap('');
+	    context.$element().append('<h1>View League</h1>');
+	});
+
+	this.get('#/create_league', function(context) {
+	    context.app.swap('');
+	    context.$element().append('<h1>Create League</h1>');
+	});
+
+	this.get('#/edit_league', function(context) {
+	    context.app.swap('');
+	    context.$element().append('<h1>Edit League</h1>');
+	});
+
+	this.get('#/delete_league', function(context) {
+	    context.app.swap('');
+	    context.$element().append('<h1>Delete League</h1>');
+	});
+
+});
+
